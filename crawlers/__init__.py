19208870ea8c4b170579d5fe0df739fe7d8fc541
Brandyn A. White <bwhite@dappervision.com>
2013-02-05 17:37:26 -0500

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

---
+++ crawlers/__init__.py
@@ -0,0 +1,142 @@
+import requests
+import urllib
+import urllib2
+import httplib
+import json
+import glob
+import os
+import cPickle as pickle
+import hadoopy_hbase
+import xml.parsers.expat
+import xml.etree.ElementTree
+import os
+import sys
+import hashlib
+
+
+class HBaseCrawlerStore(object):
+
+    def __init__(self, row_prefix):
+        self.hb = hadoopy_hbase.connect()
+        self.table = 'images'
+        self.row_prefix = row_prefix
+        self.random_bytes = 10
+
+    def store(self, image, class_name, source, **kw):
+        cols = []
+        md5 = lambda x: hashlib.md5(x).digest()
+        add_col = lambda x, y: cols.append(hadoopy_hbase.Mutation(column=x, value=y))
+        add_col('data:image', image)
+        add_col('meta:class', class_name)
+        add_col('meta:source', source)
+        add_col('hash:md5', md5(image))
+        for x, y in kw.items():
+            add_col('meta:' + x, y)
+        row = self.row_prefix + os.urandom(self.random_bytes)
+        self.hb.mutateRow(self.table, row, cols)
+
+
+def _verify_image(image_binary):
+    import imfeat
+    try:
+        imfeat.image_fromstring(image_binary)
+    except IOError:
+        return False
+    return True
+
+
+def _crawl_wrap(crawler):
+
+    def inner(store, class_name, query=None, *args, **kw):
+        query = query if query is not None else class_name
+        results = crawler(query, *args, **kw)
+        num_results = 0
+        for result in results:
+            store.store(class_name=class_name, **result)
+            num_results += 1
+        return num_results
+    return inner
+
+
+def _google_crawl(query, api_key):
+    query = urllib.quote(query)
+    result = json.loads(requests.get('https://www.googleapis.com/customsearch/v1?key=%s&cx=011323180791009162744:boa0ofeir5k&q=%s&searchType=image&imgType=clipart' % (api_key, query), verify=False).content)
+    for x in result['items']:
+        try:
+            r = requests.get(x['link'], timeout=1)
+            image = r.content
+            if r.status_code != 200:
+                continue
+        except (requests.exceptions.TooManyRedirects, requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+            continue
+        if not _verify_image(image):
+            continue
+        yield {'source': 'google',
+               'snippet': x['snippet'].encode('utf-8'),
+               'url': x['link'].encode('utf-8'),
+               'query': query.encode('utf-8')}
+
+
+def _flickr_crawl(query, api_key, api_secret, min_upload_date=None, max_upload_date=None, page=None, has_geo=False):
+    import flickrapi
+    flickr = flickrapi.FlickrAPI(api_key, api_secret)
+    try:
+        kw = {}
+        if min_upload_date is not None:
+            kw['min_upload_date'] = min_upload_date
+        if max_upload_date is not None:
+            kw['max_upload_date'] = max_upload_date
+        if page is not None:
+            kw['page'] = page
+        if has_geo:
+            kw['has_geo'] = 1
+        extras = 'description,license,date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o'
+        print(query)
+        res = flickr.photos_search(text=query,
+                                   extras=extras,
+                                   per_page=500,
+                                   **kw)
+    except (httplib.BadStatusLine,
+            flickrapi.exceptions.FlickrError,
+            xml.parsers.expat.ExpatError,
+            xml.etree.ElementTree.ParseError,
+            urllib2.URLError,
+            urllib2.HTTPError), e:
+        # TODO: Handle exceptions
+        print(e)
+        return
+    else:
+        for photo in res.find('photos'):
+            photo = dict(photo.items())
+            print(photo)
+            try:
+                print(photo['url_m'])
+                out = {'source': 'flickr', 'url': photo['url_m']}
+            except KeyError:
+                continue
+
+            def _get_data(key):
+                try:
+                    out[key] = photo[key].encode('utf-8')
+                except KeyError:
+                    pass
+            print(photo)
+            for key in ['title', 'tags', 'latitude', 'longitude',
+                        'accuracy', 'dateupload', 'datetaken']:
+                _get_data(key)
+            print(out)
+            try:
+                r = requests.get(out['url'], timeout=1)
+            except (requests.exceptions.TooManyRedirects, requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+                continue
+            out['image'] = r.content
+            if r.status_code != 200:
+                continue
+            # Unavailable and other unrelated images are GIFs, skip them
+            if out['image'].startswith('GIF87'):
+                continue
+            yield out
+
+
+google_crawl = _crawl_wrap(_google_crawl)
+flickr_crawl = _crawl_wrap(_flickr_crawl)
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='crawlers',
+      version='0.0.1',
+      packages=['crawlers'],
+      author='Brandyn A. White',
+      author_email='bwhite@dappervision.com',
+      license='GPL')
